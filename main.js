e5948ae2f46b5c395707fdaba1bfb728b24b142c
jumi sanchez <jumary.sanchez@codeimmersives.com>
2020-04-08 15:33:53 -0400

    array

--- main.js
+++ main.js
@@ -9,25 +9,33 @@ const Ray = () => {
       },
 
       pop:function(param1){
+
         this.length = this.length -1  
+        for (let i = this.length-1;i > 0; i--){
+          this[i] = this[i-1]
+        }
         this[this.length]= param1
+        
         
         },
 
-        unshift:function(param1){
-          this.length = this.length +1  
-          this[this.length]= param1
-          this[0]= param1
-          this[param1] 
-
-        
-          
-          },
-          shift:function(param1){
-            this.length = this.length -1  
-            this[this.length]= param1
-            
-            }
+        unshift: function(value){
+          this.length = this.length +1
+          for (let i = this.length-1;i > 0; i--){
+            this[i] = this[i-1]
+          }
+          this[0]=value
+        },
+        shift: function(){
+          const deleted = this[0]
+          for (let i = 0;i < this.length; i++){
+            this[i] = this[i+1]
+          }
+    
+          delete this[this.length-1]
+          this.length = this.length - 1
+          return deleted
+        },
 
 
   }
