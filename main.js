46ec5068a4c1bf638d1100a75b085f7edd7f0c09
jumi sanchez <jumary.sanchez@codeimmersives.com>
2020-04-08 14:46:42 -0400

    ray

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
 const Ray = () => {
-  
-}
+  const myRay = {
+    length:0,
+    
+    push:function(param1){
+      this.length = this.length +1  
+      this[this.length]= param1
+      
+      },
 
+      pop:function(param1){
+        this.length = this.length -1  
+        this[this.length]= param1
+        
+        },
+
+        unshift:function(param1){
+          this.length = this.length +1  
+          this[this.length]= param1
+          this[0]= param1
+          this[param1] 
+
+        
+          
+          },
+          shift:function(param1){
+            this.length = this.length -1  
+            this[this.length]= param1
+            
+            }
+
+
+  }
+  
+  return myRay
+
+}
 module.exports = {
   Ray,
 }
